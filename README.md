a389b5d3f9e6d5adc8095a922b7b3c5d0504ea09
luis <lrlitang18@gmail.com>
2023-06-29 11:46:21 -0500

    fix: update new features changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# git
+# model-house
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['ModelHouseOfficialLogo.png',
+      'banner.png','src/assets/area_card.png', 'src/assets/room_card.png', 'src/identity-and-access-management']
     }
   }
 })
